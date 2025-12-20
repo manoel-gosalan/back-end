1c6e83003af92d40087c96c7618b566e204a3569
Manoel Lidio Gosalan dos Santos <manoel.gosalan@outlook.com>
2025-12-20 00:26:26 +0000

    feat: Conclusão do desafio 4

--- Python/M1/desafios/df04.py
+++ Python/M1/desafios/df04.py
@@ -1,3 +1,16 @@
 '''
 Faça um script que leia algo na tela e mostre o sey tipo primitivo e todas as informações possiveis sobre ele.
 '''
+
+digitacao_do_usuario = input('Digite algo: ')
+
+print(f'o tipo primitivo de {digitacao_do_usuario} é: {type(digitacao_do_usuario)}')
+print(f'    É numérico? {digitacao_do_usuario.isnumeric()}')
+print(f'    É alfabético? {digitacao_do_usuario.isalpha()}')
+print(f'    É alfanumérico? {digitacao_do_usuario.isalnum()}')
+print(f'    Está em maiúscula? {digitacao_do_usuario.isupper()}')
+print(f'    Está em minúscula? {digitacao_do_usuario.islower()}')
+print(f'    Começa com maiúscula? {digitacao_do_usuario.istitle()}')
+print(f'    Só tem espaços? {digitacao_do_usuario.isspace()}')
+
+
